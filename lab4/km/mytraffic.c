70282f2547410739198bcb45ec9408c5d2cbcb6d
Justin Sadler <jsad@bu.edu>
2023-03-18 18:59:22 -0400

    Used gpio legacy API. Added button interrupts

--- lab4/km/mytraffic.c
+++ lab4/km/mytraffic.c
@@ -1,9 +1,14 @@
-// Name: Justin Sadler, Abin George // Date: 17-03-2023 
+// Name: Justin Sadler, Abin George 
+// Date: 17-03-2023 
 /*
 Sources:
 	https://perma.cc/4GWJ-A7C7
 	https://www.kernel.org/doc/html/v4.19/driver-api/gpio/consumer.html
+	https://www.kernel.org/doc/html/v4.19/driver-api/gpio/legacy.html
 	Linux Device Drivers 3rd Edition: Chapter 10 (Interrupt Handling)
+	https://embetronicx.com/tutorials/linux/device-drivers/linux-device-driver-tutorial-part-13-interrupt-example-program-in-linux-kernel/
+	https://www.kernel.org/doc/html/v4.19/core-api/kernel-api.html#c.free_irq
+
  */
 
 
@@ -20,20 +25,26 @@ Sources:
 #include <asm/system_misc.h> /* cli(), *_flags */
 #include <linux/uaccess.h>
 #include <asm/uaccess.h> /* copy_from/to_user */
+#include <linux/gpio.h> // legacy GPIO interface
 #include <linux/gpio/consumer.h> // Gpio consumer interface
+#include <linux/interrupt.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
 
 // GPIO Numbers
-#define RED_LED_GPIO  67 
-#define YELLOW_LED_GPIO 68 
-#define GREEN_LED_GPIO  44 
-#define BTN0_GPIO  26
-#define BTN1_GPIO  46 
+#define RED_LED  67 
+#define YELLOW_LED 68 
+#define GREEN_LED  44 
+#define BTN0  26
+#define BTN1  46 
 
 #define DEBUG 1
 
-/* Declaration of memory.c functions */
+
+MODULE_AUTHOR("Abin George, Justin Sadler");
+MODULE_DESCRIPTION("Traffic Light Driver");
+MODULE_LICENSE("GPL");
+
+/* Declaration of mytraffic.c functions */
 static int mytraffic_open(struct inode *inode, struct file *filp);
 static int mytraffic_release(struct inode *inode, struct file *filp);
 static ssize_t mytraffic_read(struct file *filp,
@@ -42,6 +53,9 @@ static ssize_t mytraffic_write(struct file *filp,
 		const char *buf, size_t count, loff_t *f_pos);
 static void mytraffic_exit(void);
 static int mytraffic_init(void);
+
+static irqreturn_t btn0_handler(int irq, void * dev_id);
+static irqreturn_t btn1_handler(int irq, void * dev_id);
 
 /* Structure that declares the usual file */
 /* access functions */
@@ -52,6 +66,14 @@ struct file_operations mytraffic_fops = {
 	release: mytraffic_release
 };
 
+// Operation mode enum
+enum OperationalMode {
+	NORMAL,
+	FLASHING_RED,
+	FLASHING_YELLOW
+};
+
+
 /* Declaration of the init and exit functions */
 module_init(mytraffic_init);
 module_exit(mytraffic_exit);
@@ -59,27 +81,31 @@ module_exit(mytraffic_exit);
 /* Global variables of the driver */
 /* Major number */
 static int mytraffic_major = 61;
+// Mode of traffic light
+static enum OperationalMode mode;
+// 
+static int pedestrian_called = 0;
 
-/* Buffer to store data */
-static char *mytraffic_buffer;
-/* length of the current message */
-static int mytraffic_len;
-static int capacity = 128;
-static int bite = 4;
+// BTN0 irq
+static int btn0_irq;
+// BTN1 irq
+static int btn1_irq;
 
-// GPIO Descriptors
-struct gpio_desc 
-	* RED_LED, 
-	* YELLOW_LED,
-	* GREEN_LED, 
-	* BTN0, 
-	* BTN1
-; 
+// GPIO Array
+static struct gpio gpios[] = {
+	{RED_LED,	 	GPIOF_OUT_INIT_LOW,	"Red LED" }, /* default to OFF */
+	{YELLOW_LED, 	GPIOF_OUT_INIT_LOW, "Yellow LED" }, /* default to OFF */
+	{GREEN_LED, 	GPIOF_OUT_INIT_LOW, "Green LED"   }, /* default to OFF */
+	{BTN0, 		GPIOF_DIR_IN,  		"BTN0"  }, 
+	{BTN1, 		GPIOF_DIR_IN,  		"BTN1"  }
+};
 
 
 static int mytraffic_init(void)
 {
 	int result;
+	int err;
+
 
 	/* Registering device */
 	result = register_chrdev(mytraffic_major, "mytraffic", &mytraffic_fops);
@@ -90,59 +116,41 @@ static int mytraffic_init(void)
 		return result;
 	}
 
-	/* Allocating mytraffic for the buffer */
-	mytraffic_buffer = kmalloc(capacity, GFP_KERNEL); 
-	if (!mytraffic_buffer)
-	{ 
-		printk(KERN_ALERT "Insufficient kernel memory\n"); 
-		result = -ENOMEM;
+	// Request GPIO lines
+	err = gpio_request_array(gpios, ARRAY_SIZE(gpios));
+	if(err) {
+		printk(KERN_ALERT "Could not request GPIOs\n"); 
 		goto fail; 
-	} 
-	memset(mytraffic_buffer, 0, capacity);
-	mytraffic_len = 0;
-
-	// Get GPIO Descriptors
-	RED_LED = gpio_to_desc(RED_LED_GPIO);
-	YELLOW_LED = gpio_to_desc(YELLOW_LED_GPIO);
-	GREEN_LED = gpio_to_desc(GREEN_LED_GPIO);
-	BTN0 = gpio_to_desc(BTN0_GPIO);
-	BTN1 = gpio_to_desc(BTN1_GPIO);
-
-	// Set directions of LED GPIOs as output and set initial value to be 0
-	result = gpiod_direction_output(RED_LED, 0); 
-	if(result < 0) {
-		printk(KERN_ALERT
-				"mytraffic: cannot set direction of red LED\n");
 	}
 
-	result = gpiod_direction_output(YELLOW_LED, 0);
-	if(result < 0) {
-		printk(KERN_ALERT
-				"mytraffic: cannot set direction of yellow LED\n");
+	// Request IRQs
+	btn0_irq = gpio_to_irq(BTN0);
+	if(btn0_irq < 0) {
+		printk(KERN_ALERT "BTN0 can't be mapped to an interrupt line\n");
+		goto fail;
 	}
 
-	result = gpiod_direction_output(GREEN_LED, 0);
-	if(result < 0) {
-		printk(KERN_ALERT
-				"mytraffic: cannot set direction of green LED\n");
+	btn1_irq = gpio_to_irq(BTN1);
+	if(btn1_irq < 0) {
+		printk(KERN_ALERT "BTN1 can't be mapped to an interrupt line\n");
+		goto fail;
 	}
 
+	
+	err = request_irq(btn0_irq, btn0_handler, 
+			IRQF_TRIGGER_RISING, "btn0", NULL);
 
-	result = gpiod_direction_input(BTN0);
-	if(result < 0) {
-		printk(KERN_ALERT
-				"mytraffic: cannot set direction of BTN0\n");
+	if(err) {
+		printk(KERN_ALERT "Couldn't install interrupt handler for BTN0\n");
 	}
 
-	result = gpiod_direction_input(BTN1);
-	if(result < 0) {
-		printk(KERN_ALERT
-				"mytraffic: cannot set direction of BTN1\n");
+	err = request_irq(btn1_irq, btn1_handler, 
+			IRQF_TRIGGER_RISING, "btn1", NULL);
+
+	if(err) {
+		printk(KERN_ALERT "Couldn't install interrupt handler for BTN1\n");
 	}
 
-#if DEBUG
-	gpiod_set_value(RED_LED, 1);
-#endif
 	printk(KERN_ALERT "Inserting mytraffic module\n"); 
 	return 0;
 
@@ -156,15 +164,11 @@ static void mytraffic_exit(void)
 	/* Freeing the major number */
 	unregister_chrdev(mytraffic_major, "mytraffic");
 
-	/* Freeing buffer memory */
-	if (mytraffic_buffer)
-	{
-		kfree(mytraffic_buffer);
-	}
-
-#if DEBUG
-	gpiod_set_value(RED_LED, 0);
-#endif
+	free_irq(btn0_irq, NULL);
+	gpio_set_value(RED_LED, 0);
+	gpio_set_value(GREEN_LED, 0);
+	gpio_set_value(YELLOW_LED, 0);
+	gpio_free_array(gpios, ARRAY_SIZE(gpios));
 
 	printk(KERN_ALERT "Removing mytraffic module\n");
 
@@ -189,82 +193,39 @@ static int mytraffic_release(struct inode *inode, struct file *filp)
 
 static ssize_t mytraffic_read(struct file *filp, char *buf, 
 							size_t count, loff_t *f_pos)
-{ 
-	int temp;
-	char tbuf[256], *tbptr = tbuf;
-
-	/* end of buffer reached */
-	if (*f_pos >= mytraffic_len)
-	{
-		return 0;
-	}
-
-	/* do not go over then end */
-	if (count > mytraffic_len - *f_pos)
-		count = mytraffic_len - *f_pos;
-
-	/* do not send back more than a bite */
-	if (count > bite) count = bite;
-
-	/* Transfering data to user space */ 
-	if (copy_to_user(buf, mytraffic_buffer + *f_pos, count))
-	{
-		return -EFAULT;
-	}
-
-	tbptr += sprintf(tbptr,								   
-		"read called: process id %d, command %s, count %d, chars ",
-		current->pid, current->comm, count);
-
-	for (temp = *f_pos; temp < count + *f_pos; temp++)					  
-		tbptr += sprintf(tbptr, "%c", mytraffic_buffer[temp]);
-
-	printk(KERN_DEBUG "%s\n", tbuf);
-
-	/* Changing reading position as best suits */ 
-	*f_pos += count; 
-	return count; 
+{
+	return 0;
 }
 
 static ssize_t mytraffic_write(struct file *filp, const char *buf,
 							size_t count, loff_t *f_pos)
 {
-	int temp;
-	char tbuf[256], *tbptr = tbuf;
-
-	/* end of buffer reached */
-	if (*f_pos >= capacity)
-	{
-		printk(KERN_DEBUG
-			"write called: process id %d, command %s, count %d, buffer full\n",
-			current->pid, current->comm, count);
-		return -ENOSPC;
-	}
-
-	/* do not eat more than a bite */
-	if (count > bite) count = bite;
-
-	/* do not go over the end */
-	if (count > capacity - *f_pos)
-		count = capacity - *f_pos;
-
-	if (copy_from_user(mytraffic_buffer + *f_pos, buf, count))
-	{
-		return -EFAULT;
-	}
-
-	tbptr += sprintf(tbptr,								   
-		"write called: process id %d, command %s, count %d, chars ",
-		current->pid, current->comm, count);
-
-	for (temp = *f_pos; temp < count + *f_pos; temp++)					  
-		tbptr += sprintf(tbptr, "%c", mytraffic_buffer[temp]);
-
-	printk(KERN_DEBUG "%s\n", tbuf);
-
-	*f_pos += count;
-	mytraffic_len = *f_pos;
-
-	return count;
+	return 0;
 }
 
+static irqreturn_t btn0_handler(int irq, void * dev_id) {
+
+#if DEBUG
+	printk(KERN_INFO "Switching flashing modes\n");
+#endif
+
+	switch(mode) {
+		case NORMAL:
+			mode = FLASHING_RED;
+			break;
+		case FLASHING_RED:
+			mode = FLASHING_RED;
+		case FLASHING_YELLOW:
+			mode = NORMAL;
+	}
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t btn1_handler(int irq, void * dev_id) {
+
+#if DEBUG
+	printk(KERN_INFO "Pedestrian Called");
+#endif
+	pedestrian_called = 1;
+	return IRQ_HANDLED;
+}
