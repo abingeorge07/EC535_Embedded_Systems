dd17689d33f00e1231518c4823c9792e108b5c55
abingeorge07 <66330225+abingeorge07@users.noreply.github.com>
2023-05-02 20:16:01 -0400

    GUI

---
+++ lab5/rasterwindow/main.cpp
@@ -0,0 +1,14 @@
+#include "rasterwindow.h"
+
+
+//! [1]
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+    RasterWindow window;
+
+    window.show();
+
+    return app.exec();
+}
+//! [1]
